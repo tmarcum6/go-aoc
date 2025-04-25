b81041a6ed827bbf59f81a33b84e9a7ad50f4567
Taylor Marcum <marcum.taylor@gmail.com>
2025-04-25 15:09:30 -0700

    day 3 part 1 finished

--- 2024/day3/day3.go
+++ 2024/day3/day3.go
@@ -11,14 +11,11 @@ import (
 func main() {
 	input := utility.ReadFileLineByLine("../../input/day3.txt")
 
-	re := regexp.MustCompile(`mul\((-?\d+),(-?\d+)\)`) // match mul(x,y)
+	re := regexp.MustCompile(`mul\((-?\d+),(-?\d+)\)`) // mul(x,y)
 
 	var matches [][]string
-	matches = re.FindAllStringSubmatch(input[0], -1)
-
-	if len(matches) == 0 {
-		fmt.Println("no match")
-		return
+	for _, line := range input {
+		matches = append(matches, re.FindAllStringSubmatch(line, -1)...)
 	}
 
 	sum := 0
@@ -26,7 +23,7 @@ func main() {
 		x, _ := strconv.Atoi(m[1])
 		y, _ := strconv.Atoi(m[2])
 		sum += x * y
-		fmt.Printf("mul(%d, %d) = %d\n", x, y, x*y)
+		//fmt.Printf("mul(%d, %d) = %d\n", x, y, x*y)
 	}
 	fmt.Printf("sum: %d\n", sum)
 }
