cc0e7a65b4004ac956d3e922b42d2e974a541bd6
Taylor Marcum <marcum.taylor@gmail.com>
2025-04-25 13:43:31 -0700

    day 3 still stuck

--- 2024/day3/day3.go
+++ 2024/day3/day3.go
@@ -1,11 +1,32 @@
 package main
 
 import (
-//	"fmt"
+	"fmt"
+	"regexp"
+	"strconv"
 
-// utility "example.com/go/aoc/utility"
+	utility "example.com/go/aoc/utility"
 )
 
 func main() {
-	lines := utility.ReadFileLineByLine("../../input/day3.txt")
+	input := utility.ReadFileLineByLine("../../input/day3.txt")
+
+	re := regexp.MustCompile(`mul\((-?\d+),(-?\d+)\)`) // match mul(x,y)
+
+	var matches [][]string
+	matches = re.FindAllStringSubmatch(input[0], -1)
+
+	if len(matches) == 0 {
+		fmt.Println("no match")
+		return
+	}
+
+	sum := 0
+	for _, m := range matches {
+		x, _ := strconv.Atoi(m[1])
+		y, _ := strconv.Atoi(m[2])
+		sum += x * y
+		fmt.Printf("mul(%d, %d) = %d\n", x, y, x*y)
+	}
+	fmt.Printf("sum: %d\n", sum)
 }
