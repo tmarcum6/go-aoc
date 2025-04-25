e760b411656d22836be23b24321c6fca92a45bff
Taylor Marcum <marcum.taylor@gmail.com>
2025-04-25 15:40:23 -0700

    day 3 part 2 start

--- 2024/day3/day3.go
+++ 2024/day3/day3.go
@@ -10,7 +10,12 @@ import (
 
 func main() {
 	input := utility.ReadFileLineByLine("../../input/day3.txt")
+	_ = findAllMatchesPart2(input)
+	//sum := calculateSumOfMatches(matches)
+	//fmt.Printf("sum: %d\n", sum)
+}
 
+func findAllMatchesPart1(input []string) [][]string {
 	re := regexp.MustCompile(`mul\((-?\d+),(-?\d+)\)`) // mul(x,y)
 
 	var matches [][]string
@@ -18,6 +23,22 @@ func main() {
 		matches = append(matches, re.FindAllStringSubmatch(line, -1)...)
 	}
 
+	return matches
+}
+
+func findAllMatchesPart2(input []string) [][]string {
+	re := regexp.MustCompile(`mul\((-?\d+),(-?\d+)\)|do\(\)|don't\(\)`)
+
+	var matches [][]string
+	for _, line := range input {
+		matches = append(matches, re.FindAllStringSubmatch(line, -1)...)
+		fmt.Printf("matches: %s\n", line)
+	}
+
+	return matches
+}
+
+func calculateSumOfMatches(matches [][]string) int {
 	sum := 0
 	for _, m := range matches {
 		x, _ := strconv.Atoi(m[1])
@@ -25,5 +46,5 @@ func main() {
 		sum += x * y
 		//fmt.Printf("mul(%d, %d) = %d\n", x, y, x*y)
 	}
-	fmt.Printf("sum: %d\n", sum)
+	return sum
 }
