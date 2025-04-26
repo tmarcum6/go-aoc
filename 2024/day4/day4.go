7f2099bbbd148380efe9bd262106933cbe091279
Taylor Marcum <marcum.taylor@gmail.com>
2025-04-26 11:57:46 -0700

    start day 4

--- 2024/day4/day4.go
+++ 2024/day4/day4.go
@@ -2,8 +2,20 @@ package main
 
 import (
 	"fmt"
+	"regexp"
+
+	utility "example.com/go/aoc/utility"
 )
 
 func main() {
+	input := utility.ReadFileLineByLine("../../input/day4.txt")
+	re := regexp.MustCompile(`xmas`)
+	var matches [][]string
+	for _, line := range input {
+		matches = append(matches, re.FindAllStringSubmatch(line, -1)...)
+	}
+	for _, m := range matches {
+		fmt.Println(m)
+	}
 
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module example.com/go/aoc
 
 go 1.24.2
 
-replace example.com/go/aoc => ../../go/aoc
+//replace example.com/go/aoc => ../../go/aoc
