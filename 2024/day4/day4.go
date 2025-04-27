b9f63407f35a5aeb6dff2fc4f3c43ebffbf16ceb
Taylor Marcum <marcum.taylor@gmail.com>
2025-04-27 13:23:38 -0700

    day 4 work

--- 2024/day4/day4.go
+++ 2024/day4/day4.go
@@ -7,15 +7,23 @@ import (
 	utility "example.com/go/aoc/utility"
 )
 
+// parse east
+// parse west
+// parse north
+// parse south
+// parse southeast
+// parse southwest
+// parse northwest
+// parse northeast
+
 func main() {
 	input := utility.ReadFileLineByLine("../../input/day4.txt")
-	re := regexp.MustCompile(`xmas`)
+	re := regexp.MustCompile(`XMAS`)
 	var matches [][]string
+	count := 0
 	for _, line := range input {
 		matches = append(matches, re.FindAllStringSubmatch(line, -1)...)
 	}
-	for _, m := range matches {
-		fmt.Println(m)
-	}
 
+	fmt.Printf("count: %d\n", count)
 }
